445f056c84022e3fc5d112473f6fc8b55604cac8
Bryan <suprnewb@gmail.com>
2022-12-15 18:13:26 +1100

    Create task code

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
 import React, {useState} from "react";
 import Header from "./components/Header";
-import ActiveTasks from "./components/Tasklist";
+import Tasklist from "./components/Tasklist";
 import Footer from "./components/Footer";
 
 function App() {
-  const [tasks, setTasks] = useState([
-  {
-    title: "Wash Car",
-    completed: false
-  },
-  {
-    title: "Vacuum Car",
-    completed: false
-  },
-  {
-    title: "Wax Car",
-    completed: false
-  },
-  {
-    title: "Walk the dog",
-    completed: true
-  }
-  ]);
-  
   return (
     <div className="App">
       <Header />
-      <ActiveTasks 
-      tasks = {tasks}
-      setTasks = {setTasks}
-      />
+      <Tasklist />
       <Footer />
     </div>
   );
--- src/components/Tasklist.js
+++ src/components/Tasklist.js
@@ -1,23 +1,55 @@
-import React, {useCallback} from "react";
+import React, {useCallback, useState, useEffect} from "react";
 
-function ActiveTasks({tasks, setTasks}) {
+function Tasklist() {
+  const [tasks, setTasks] = useState([
+    {
+      title: "Wash Car",
+      completed: false
+    },
+    {
+      title: "Vacuum Car",
+      completed: false
+    },
+    {
+      title: "Wax Car",
+      completed: false
+    },
+    {
+      title: "Walk the dog",
+      completed: true
+    }
+    ]);
 
   const addTask = useCallback(() => {
-    let taskInput = document.getElementsByClassName("taskInput")[0]
+    const taskInput = document.getElementsByClassName("taskInput")[0];
     setTasks((t) => [...t, {title: taskInput.value, completed: false}]);
     taskInput.value = "";
   },[setTasks])
 
+  const completeTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks[index].completed = true;
+    setTasks(newTasks);
+  }
+
+  const delTask = index => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1)
+    setTasks(newTasks);
+  };
+
   return (
-    <div className="task-list">
-      <input type="text" className="taskInput" placeholder="Add new task"></input>
-      <button type="submit" className="addButton" htmlFor="taskInput" onClick={addTask}>+</button>
-      <div className="active-tasks">
-        {tasks.map((item, i) => {
+    <div className="taskContainer">
+      <input type="text" className="taskInput" placeholder="Add new task" required></input>
+      <button type="submit" className="addBtn" htmlFor="taskInput" onClick={addTask}>+</button>
+      
+      <div className="taskList">
+        {tasks.map((item, index) => {
           return (
-            <div key={i}>
-              <input type="checkbox" className={item.title}></input>
-              <label htmlFor={item.title}>{item.title}</label>
+            <div key={index} className={tasks[index].completed ? "completed" : "active"}>
+              <label htmlFor={item.title} className="taskLabel">{item.title}</label>
+              <button className="completeBtn" index={index} onClick={() => completeTask(index)}>√</button>
+              <button className="deleteBtn" index={index} onClick={delTask}>x</button>
             </div>
           )
         })}
@@ -26,4 +58,4 @@ function ActiveTasks({tasks, setTasks}) {
   );
 }
 
-export default ActiveTasks;
+export default Tasklist;
