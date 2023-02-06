cb9b7ee0fac1ae0657aa794b1f6afc36cb35e882
Bryan <suprnewb@gmail.com>
2023-02-06 18:47:37 +1100

    ESLint and prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import Header from "./components/Header";
 import Tasklist from "./components/Tasklist";
 import Footer from "./components/Footer";
---
+++ src/components/Tasklist.jsx
@@ -0,0 +1,103 @@
+/* eslint-disable arrow-body-style */
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+function Tasklist() {
+  const [newTask, setNewTask] = useState("");
+  const newId = uuidv4();
+
+  const [tasks, setTasks] = useState([
+    {
+      title: "Wash Car",
+      completed: false,
+    },
+    {
+      title: "Vacuum Car",
+      completed: false,
+    },
+    {
+      title: "Wax Car",
+      completed: false,
+    },
+    {
+      title: "Walk the dog",
+      completed: true,
+    },
+  ]);
+
+  const addTask = () => {
+    if (!newTask) return;
+
+    setTasks([...tasks, { title: newTask, completed: false }]);
+    setNewTask("");
+  };
+
+  const handleNewTaskChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const completeTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks[index].completed = true;
+    setTasks(newTasks);
+  };
+
+  const delTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
+  return (
+    <div>
+      <div className="inputContainer">
+        <input
+          type="text"
+          className="taskInput"
+          placeholder="Add new task"
+          onChange={handleNewTaskChange}
+          value={newTask}
+          required
+        />
+        <button
+          type="submit"
+          className="addBtn"
+          htmlFor="taskInput"
+          onClick={addTask}
+        >
+          +
+        </button>
+      </div>
+      <div className="taskContainer">
+        {tasks.map((item, index) => {
+          return (
+            <div
+              key={newId}
+              className={tasks[index].completed ? "completed" : "active"}
+            >
+              <div className="taskLabel">{item.title}</div>
+              <div className="edit">
+                <button
+                  type="button"
+                  className="completeBtn"
+                  onClick={() => completeTask(index)}
+                >
+                  √
+                </button>
+                <button
+                  type="button"
+                  className="deleteBtn"
+                  onClick={() => delTask(index)}
+                >
+                  x
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Tasklist;
